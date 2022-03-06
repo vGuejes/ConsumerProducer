5294e282abf9589ae12c7f7a5a93fde1b2e9cd31
vGuejes <98520280+vGuejes@users.noreply.github.com>
2022-03-06 23:45:55 +0100

    Add files via upload

--- consumer_producer.py
+++ consumer_producer.py
@@ -1,175 +1,270 @@
-#!/usr/bin/env python3
-# -*- coding: utf-8 -*-
-"""
-Created on Thu Feb 24 10:07:40 2022
-
-@author: alumno
-"""
-
-
-#Implementar un merge concurrente:
-#- Tenemos NPROD procesos que producen números no negativos de forma
-#creciente. Cuando un proceso acaba de producir, produce un -1
-#
-#- Hay un proceso merge que debe tomar los números y almacenarlos de
-#forma creciente en una única lista (o array). El proceso debe esperar a que
-#los productores tengan listo un elemento e introducir el menor de
-#ellos.
-#
-#- Se debe crear listas de semáforos. Cada productor solo maneja los
-#sus semáforos para sus datos. El proceso merge debe manejar todos los
-#semáforos.
-#
-#- OPCIONAL: mente se puede hacer un búffer de tamaño fijo de forma que
-#los productores ponen valores en el búffer.
-
-
-from multiprocessing import Process, Manager
+from multiprocessing import Process
 from multiprocessing import BoundedSemaphore, Semaphore, Lock
 from multiprocessing import current_process
-from multiprocessing import Array#, Value
+from multiprocessing import Array
 from time import sleep
 from random import random, randint
 
-NCONS = 1 # la cantidad de consumidores es 1
 NPROD = 3 # la cantidad de productores es P
-N = 100 # la cantidad de productos que va a producir cada productor
+N = 10 # la cantidad de productos que va a producir cada productor
 K = 1 # el k del bounded semaphore (la capacidad de almacenamiento)
-
+# Esta practica solo funciona para k=1 (no la parte opcional)
 
 def delay(factor = 3):
     sleep(random()/factor)
 
 
-def add_data(storage, pid, data, mutex):
+def add_data(storage, pid,mutex):
+    '''
+    Esta funcion anade al almacen del productor pid el sigiente numero generado
+    por el productor de id pid. Para ello, necesita que el almacen tenga espacio
+    (el semaforo empties[pid] debe estar abierto)
+
+    ENTRADA:
+    storage: Almacen compartido
+    pid: ID del productor, entero entre 0 y NPROD-1
+    mutex: Semaforo binario para la exclusion mutua de las diferentes secciones criticas
+
+    FUNCIONAMIENTO:
+    Como es la seccion critica del productor, usamos mutex para no entrar en otras
+    secciones criticas
+    Su funcionamiento es tomar el anterior valor generado (storage[pid]), sumarle
+    un entero aleatorio entre 0 y 5 y almacenarlo de nuevo en storage[pid]
+    '''
     mutex.acquire()
     try:
         storage[pid] = storage[pid] + randint(0,5)
         delay(6)
-        '''storage[index.value] = pid*1000 + data
-        delay(6)
-        index.value = index.value + 1'''
     finally:
         mutex.release()
 
 
-def producer(storage, empty, non_empty, mutex):
-    for _ in range(N): # produces N veces en total
-        print (f"producer {current_process().name} produciendo")
-        delay(6)
-        empty.acquire()
-        add_data(storage, int(current_process().name.split('_')[1]), mutex)
-        non_empty.release()
-        #print (f"producer {current_process().name} almacenado")
+def get_min(storage,mutex,running):
+    '''
+    Esta funcion toma el menor valor del almacen. Para ello, necesita que
+    todos los productores hayan almacenado algun valor (todos los semaforos
+    non_empties deben estar abiertos)
 
-'''
-def producer(storage, index, empty, non_empty, mutex):
-    for v in range(N):
-        print (f"producer {current_process().name} produciendo")
-        delay(6)
-        empty.acquire()
-        add_data(storage, index, int(current_process().name.split('_')[1]),
-                 v, mutex)
-        non_empty.release()
-        print (f"producer {current_process().name} almacenado {v}")
-'''
-def min_raro(n,idn,m,idm): # queremos que devuelva el minimo y el indice del minimo
-    if n == -1 and m == -1:
-        result, idr = -1, -1
-    elif n == -1: # n == -1 != m
-        result, idr = m, idm
-    elif m == -1:
-        result, idr = n, idn # m == -1 != n
-    else: # m != -1 != n
-        if n > m:
-            result, idr = m, idm
+    Ademas, esta funcion actualiza el vector running si es necesario
+
+    ENTRADA:
+    storage: Almacen compartido
+    mutex: Semaforo binario para la exclusion mutua de las diferentes secciones criticas
+    running: vector de booleanos en el que running[i] indica si el productor de
+        pid = i ha terminado de producir
+    
+    SALIDA:
+    minimo: el valor minimo de storage
+    idmin: el indice del productor al que pertenece minimo
+        relacion: minimo = storage[idmin]
+    
+    FUNCIONAMIENTO:
+    como es la seccion critica del consumidor, usamos mutex para no entrar en otras
+        secciones criticas
+    inicializamos idmin y minimo a -1 por si falla algo en esta funcion,
+        lo sepamos desde fuera
+    inicializamos la lista de los indices validos (los que storage[i]=!-1)
+    para cada indice i que verifique running[i]=True, comprobamos storage[i]
+        y lo cambiamos si procede. Si no procede, lo añadimos a la lista de 
+        indices validos
+    por ultimo, tomamos el menos valor de storage[i] para los indices validos
+        y devolvemos tanto el indice como el propio valor
+    
+    ademas, en el ultimo momento cuando storage=[-1,...,-1], esta funcion es
+    la que convierte running a [False] para que termine el consumidor
+    '''
+    mutex.acquire()
+    try:
+        idmin, minimo = -1, -1
+        id_val = [] # indices validos
+        for i in range(NPROD):
+            if running[i]:
+                if storage[i] == -1:
+                    running[i] = False
+                else:
+                    id_val.append(i)
+        if len(id_val) == 1:
+            idmin = id_val[0]
+            minimo = storage[idmin]
+        elif len(id_val) > 1:
+            idmin = id_val[0]
+            minimo = storage[idmin]
+            for i in range(1,len(id_val)):
+                j = id_val[i]
+                if minimo > storage[j]:
+                    idmin = j
+                    minimo = storage[j]
         else:
-            result, idr = n, idn
-    return result, idr
-        
-
-def get_min(storage, index, mutex):
-    mutex.acquire()
-    try:
-        for i in range(NPROD-1):
-            data, idd = min_raro(storage[i],i,storage[i+1],i+1)
-            delay()
-        '''data = storage[0]
-        index.value = index.value - 1
+            running = [False]
         delay()
-        for i in range(index.value):
-            storage[i] = storage[i + 1]
-        storage[index.value] = -1'''
     finally:
         mutex.release()
-    return data, idd
-'''
-def get_data(storage, index, mutex):
-    mutex.acquire()
-    try:
-        data = storage[0]
-        index.value = index.value - 1
-        delay()
-        for i in range(index.value):
-            storage[i] = storage[i + 1]
-        storage[index.value] = -1
-    finally:
-        mutex.release()
-    return data
-'''
+    return minimo, idmin
 
-def consumer(storage, index, empties, non_empties, mutex, merge):
+
+def producer(storage,empty,non_empty,mutex):
+    '''
+    Proceso que simula a cada productor. El productor produce N veces
+    un entero no negativo y por ultimo produce un -1.
+    Cada elemento que produce lo guarda en storage[pid]
+
+    ENTRADA:
+    storage: Almacen compartido
+    empty: Decide si el productor puede producir: si el almacen esta lleno, el
+        productor no puede producir
+    non_empty: Avisa al consumidor de que puede consumir: Si el almacen se queda
+        vacio, entonces el consumidor no puede comparar entre todos los almacenes
+        para consumir
+    mutex: Semaforo binario para la exclusion mutua de las diferentes secciones criticas
+
+    VARIABLES:
+    pid: ID del productor, entero entre 0 y NPROD-1
+
+    FUNCIONAMIENTO:
+    El proceso crea y guarda en el almacen storage[pid] N enteros no negativos
+    que crecen. Para poder crear, necesita que haya espacio en el almacen (que
+    empty este abierto), y como el espacio del almacen es K=1, siempre que produce
+    tiene que avisar de que el almacen esta lleno (cierra non_empty)
+
+    Al final, como ultima seccion critica del productor, deja -1 en el almacen
+    para marcar que ha terminado
+    '''
+    pid = int(current_process().name.split('_')[1])
     for _ in range(N):
-        for ne in non_empties:
-            ne.acquire()
-        print (f"consumer {current_process().name} desalmacenando")
-        dato, idd = get_min(storage, index, mutex)
-        merge.append(dato)
-        empties[idd].release() # hacemos signal al productor del que hemos tomado
-                               # para que pueda producir
-        print (f"consumer {current_process().name} consumiendo {dato}")
-        delay()
+        print(f'productor {pid} produciendo')
+        delay(6)
+        empty.acquire()
+        add_data(storage,pid,mutex)
+        non_empty.release()
+        print(f'productor {pid} almacenando')
+    print(f"productor {pid} termino de producir")
+    empty.acquire()
+    mutex.acquire()
+    try:
+        storage[pid] = -1 # cuando termina de producir
+    finally:
+        mutex.release()
+    non_empty.release()
+
+
+def ordenado(l):
+    # devuelve si una lista esta ordenada de menor a mayor
+    esta_ordenada = True
+    i=0
+    long = len(l)
+    while esta_ordenada and i<long-1:
+        esta_ordenada = l[i]<=l[i+1]
+        i+=1
+    return esta_ordenada
+
+
+def consumer(storage,empties,non_empties,mutex,merge):
+    '''
+    Proceso que simula al consumidor.
+    En esta practica, el consumidor consume del almacen cuando todos los
+    productores han consumido (todos los semaforos non_empty estan abiertos)
+    y consume el menor valor no negativo del almacen
+    El proceso termina cuando todos los productores han producido N elementos
+    y el almacen es de la forma [-1,...,-1]
+
+    ENTRADA:
+    storage: Almacen compartido
+    empties: Lista de los semaforos empty de los productores
+    non_empties: Lista de los semaforos non_ empty de los productores
+    mutex: Semaforo binario para la exclusion mutua de las diferentes secciones criticas
+    merge: Lista donde dejaremos los valores consumidos. La practica sera correcta
+        si merge contiene todos los valores producidos por los productores ordenada
+        de menor a mayor
+    
+    VARIABLES:
+    it: Cantidad de iteraciones que hace el bucle. Al final de la practica, por como
+        esta programada, debe terminar con it = N*NPROD
+    running: vector de booleanos en el que running[i] indica si el productor de
+        pid = i ha terminado de producir
+    
+    FUNCIONAMIENTO:
+    mientras algun productor este produciendo, el consumidor esperara a que todos los
+        que aun estan produciendo produzcan. Como consumimos de uno en uno y los almacenes
+        tienen tamaño 1
+        (esto se traduce en que cada vez que consumimos tenemos que avisar al productor
+        del que hemos consumido que tiene que producir y una vez produce, podemos volver
+        a consumir)
+    tomamos el menor dato y el pid del productor que lo produjo
+    anadimos el dato a merge
+    avisamos al productor pid que tiene que producir
+    esperamos a que el productor pid produzca para poder comparar
+
+    una vez termina el bucle, enseñamos en pantalla la lista final merge con todos los
+    elementos producidos ordenados y una confirmacion de que esta ordenada
+    '''
+    it = 0 # iteraciones del bucle
+    for ne in non_empties:
+        ne.acquire()
+    
+    running = [True] * NPROD
+
+    while True in running:
+        it += 1
+        print("consumer desalmacenando")
+        dato, idd = get_min(storage,mutex,running)
+        if idd != -1:
+            if dato != -1:
+                merge.append(dato)
+                print(f"consumer consumiendo {dato} de prod_{idd} ({it})")
+                empties[idd].release()
+                non_empties[idd].acquire()
+        else:
+            running = [False]
+    print(merge, len(merge))
+    orden = ordenado(merge)
+    print(f"La lista merge esta ordenada: {orden}")
+
 
 def main():
-    storage = Array('i', K*NPROD)
-    #index = Value('i', 0)           # no nos hace falta porque K = 1
+    '''
+    VARIABLES:
+    storage: Almacen compartido, tiene tamaño K*NPROD = NPROD
+    merge: Lista donde dejaremos los valores consumidos. La practica sera correcta
+        si merge contiene todos los valores producidos por los productores ordenada
+        de menor a mayor
+    empties: Lista de los semaforos empty de los productores
+    non_empties: Lista de los semaforos non_ empty de los productores
+    mutex: Semaforo binario para la exclusion mutua de las diferentes secciones criticas
+    prodlist: lista de productores, tiene tamaño NPROD
+    conslist: lista de consumidores, tiene tamaño 1
+    '''
+    storage = Array('i',K*NPROD)
     for i in range(K*NPROD):
-        storage[i] = 0              # elegimos que el primer valor sea 0
-    #print ("almacen inicial", storage[:], "indice", index.value)
-    
-    manager = Manager()
-    merge = manager.list()
-    #index = Value('i', 0)   # indice del merge
-    
-    '''
-    manager1 = Manager()
-    manager2 = Manager()
-    non_empties = manager1.list() # lista de semaforos non_empties de tamaño <- deberia ser array
-    empties = manager2.list()
-    '''
+        storage[i] = 0 # el primer valor de storage sera 0 + randint(0,5)
+    print("almacen inicial", storage[:])
+
+    merge=[]
+
     non_empties = []
     empties = []
+
     for i in range(NPROD):
         non_empties.append(Semaphore(0))
         empties.append(BoundedSemaphore(K))
+
     mutex = Lock()
 
-    prodlst = [ Process(target=producer,
-                        name=f'prod_{i}',
-                        #args=(storage, index, empties[i], non_empties[i], mutex))
-                        args=(storage, empties[i], non_empties[i], mutex))
-                for i in range(NPROD) ]
-
-    conslst = [ Process(target=consumer,
-                      name="cons",
-                      #args=(storage, index, empties, non_empties, mutex, merge)) ]
-                      args=(storage, empties, non_empties, mutex, merge)) ]
-
-    for p in prodlst + conslst:
+    prodlist = [ Process(target=producer,
+                         name=f'prod_{i}',
+                         args=(storage, empties[i], non_empties[i], mutex))
+                 for i in range(NPROD) ]
+    
+    conslist = [ Process(target=consumer,
+                         name='cons',
+                         args=(storage, empties, non_empties, mutex, merge))]
+                # igual se puede quitar el manager de merge
+    
+    for p in prodlist + conslist:
         p.start()
-
-    for p in prodlst + conslst:
+    
+    for p in prodlist + conslist:
         p.join()
+
 
 
 if __name__ == '__main__':
